e22417968998acf8d1b3c7d3fbebd680d1ee0b9b
mittul <conanmittul@gmail.com>
2023-06-13 17:23:06 +0530

    Unnecessary imports removed

--- auction-service/src/main/java/com/biddingserver/auctionservice/controller/AuctionController.java
+++ auction-service/src/main/java/com/biddingserver/auctionservice/controller/AuctionController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
